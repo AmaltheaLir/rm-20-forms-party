25115ee9b5750beb23164d092cb835f2541870aa
AmaltheaLir <amalthealir@protonmail.com>
2023-02-09 17:44:08 +0100

    end for today
    see notes in app! Need to relearn the csv stuff!

--- party-app.rb
+++ party-app.rb
@@ -9,3 +9,10 @@ get '/party' do
   puts "Let's Have a Party!"
   erb :party 
 end
+
+post '/party' do
+  puts params  #can store data from form somewhere here
+  erb :party
+end
+
+# need a break... ok, need to relearn about the whole csv files shebang. Have the basic form 'figured out'. Need to also figure out how to make the csv output print neatly on the party.erb view---probably that's part of the csv shebang. 
