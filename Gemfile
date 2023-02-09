822ac4ab0af1c80c7b1a7f59ee9e5f66fe696820
AmaltheaLir <amalthealir@protonmail.com>
2023-02-09 15:12:45 +0100

    added more setup
    continuing setup while reading assignment instructions

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'sinatra', '~> 3.0', '>= 3.0.5'
---
+++ party-app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+get '/' do
+  'Hello World'
+end
+
+get '/home' do
+  "Let's Have a Party!"
+
+end
