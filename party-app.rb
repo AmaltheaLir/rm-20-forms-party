890c7bf13de810209b12d55f7feba5cf5abbbb60
AmaltheaLir <amalthealir@protonmail.com>
2023-03-20 21:40:22 +0100

    finished everything
    finished with the help from RM <3
    Added some decoration

--- party-app.rb
+++ party-app.rb
@@ -11,17 +11,16 @@ get '/party' do
   erb :party 
 end
 
-get '/party/:guests' do
-  @guests = CSV.read("guests.csv", headers: true)
-
-  @guest = params["guests"]
-
-  erb :party
-end
-
 post '/party' do
-  puts params  #can store data from form somewhere here
-  erb :party
+  name = params['name_entered']
+  friends = params['number_entered']
+  time = params['time']
+
+  CSV.open("guests.csv", "a") do |csv|
+    csv << [name, friends, time]
+  end
+   #can store data from form somewhere here  
+  redirect :party
 end
 
 # Did get the csv list to show, but not yet the columns numbers (not strings) past name. Still have to work on saving info to the csv from the form, etc.  
